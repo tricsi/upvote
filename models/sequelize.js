6aea3569f45573a2c42dac6a5845f9020ff13f00
csaba.csecskedi <csaba.csecskedi@playngo.com>
2021-09-03 14:39:27 +0200

    SSL fix

--- models/sequelize.js
+++ models/sequelize.js
@@ -3,5 +3,11 @@ const Sequelize = require('sequelize');
 const config = require('../config');
 
 module.exports = new Sequelize(config.env.DATABASE_URL, {
-  logging: config.env.DATABASE_LOG ? console.log : false
+  logging: config.env.DATABASE_LOG ? console.log : false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
